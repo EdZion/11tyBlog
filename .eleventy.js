dba6e564901810cbf372c00488ce32194c5651c9
edzion <alle020916@gmail.com>
2021-09-07 10:58:27 +0200

    🔧

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,7 @@
 module.exports = function (elventyConfig) {
+  elventyConfig.addPassthroughCopy('src/css');
+  elventyConfig.addWatchTarget('src/css');
+  
   return {
     dir: {
       input: 'src'
