e5eb7b4339dbdb314f70c170de609e364ba31f6f
edzion <alle020916@gmail.com>
2021-09-15 15:05:10 +0200

    ✅ Removed console log

--- src/_data/gitrepos.js
+++ src/_data/gitrepos.js
@@ -3,7 +3,5 @@ const axios = require('axios');
 module.exports = async () => {
     const { data } = await axios.get('https://api.github.com/users/edzion/repos');
 
-    console.log(data);
-
     return data;
 };
