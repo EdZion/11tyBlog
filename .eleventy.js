899bfc809cb17e5ea35602c682f677ccfaa36b60
edzion <alle020916@gmail.com>
2021-09-21 09:37:01 +0200

    ✨ Added highlight to code blocks

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,11 @@
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+
 module.exports = function (eleventyConfig) {
   // eleventyConfig.addPassthroughCopy("src/css");
   // eleventyConfig.addWatchTarget("src/css");
 
+  eleventyConfig.addPlugin(syntaxHighlight);
+  
   eleventyConfig.addWatchTarget("src/scss");
 
   return {
